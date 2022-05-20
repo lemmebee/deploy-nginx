33e7560a9417af1a23e0c2c4ca9b2cfa6b0da57b
ehabshaaban <ehabsfeki@gmail.com>
2022-05-20 15:21:10 +0200

    Update terraform output

--- output.tf
+++ output.tf
@@ -1,6 +1,19 @@
 output "cluster_endpoint" {
-  value = data.aws_eks_cluster.cluster.endpoint
+  description = "eks cluster endpoint"
+  value       = data.aws_eks_cluster.cluster.endpoint
 }
+
+output "cluster_name" {
+  description = "eks cluster name"
+  value       = aws_eks_cluster.this.id
+}
+
+
+output "region" {
+  description = "aws region"
+  value       = var.region
+}
+
 
 output "nginx_release_namespace" {
   description = "nginx release namespace"
